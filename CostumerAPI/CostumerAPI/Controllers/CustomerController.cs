a20aac64b161df29f0e9276323494cab663acb66
Yavuz Sarıtaş <yavuz.saritas@hangikredi.local>
2022-02-23 15:29:15 +0300

    customer api development

---
+++ CostumerAPI/CostumerAPI/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CostumerAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CustomerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("getcustomers")]
+        public async Task<IActionResult> CustomerList()
+        {
+            var customers = _configuration.GetSection("CustomerList")?.Value;
+
+            if (string.IsNullOrWhiteSpace(customers))
+                return NotFound();
+
+            var customerList = customers.Split(";", System.StringSplitOptions.RemoveEmptyEntries).ToList();
+            return Ok(customerList);
+        }
+
+        [HttpGet("status")]
+        public async Task<IActionResult> Status()
+        {
+            return Ok("Product API Run!");
+        }
+    }
+}
